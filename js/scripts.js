f2ff0860f6a68a6df26b31fb4efa123113983049
Joel Ogunremi <joelogunremi@gmail.com>
2021-04-13 18:52:30 -0400

    Display Gallery Card

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 // Display search
-console.log("Hello World");
 const searchContainer = document.querySelector(".search-container");
 let searchForm = `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
  </form>`;
 searchContainer.insertAdjacentHTML("beforeend", searchForm);
+const searchInput = document.getElementById("search-input");
+const seaarchSubmit = document.getElementById("search-submit");
+
+// Display Gallery
+const galleryDiv = document.getElementById("gallery");
+let galleryCard = `<div class="card">
+<div class="card-img-container">
+<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+</div>
+<div class="card-info-container">
+<h3 id="name" class="card-name cap">first last</h3>
+<p class="card-text">email</p>
+<p class="card-text cap">city, state</p>
+</div>
+</div>`;
+galleryDiv.insertAdjacentHTML("beforeend", galleryCard);
+
+//
