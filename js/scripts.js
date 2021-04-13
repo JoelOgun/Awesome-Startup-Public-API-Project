d7e096fbf9db970ba0adc14316d5a9a736e0bf15
Joel Ogunremi <joelogunremi@gmail.com>
2021-04-13 13:55:33 -0400

    Display search form

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Display search
+console.log("Hello World");
+const searchContainer = document.querySelector(".search-container");
+let searchForm = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+ <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+ </form>`;
+searchContainer.insertAdjacentHTML("beforeend", searchForm);
