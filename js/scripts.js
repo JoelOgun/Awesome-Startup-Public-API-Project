b8e250a784501169808e99e5814197f8f060a800
Joel Ogunremi <joelogunremi@gmail.com>
2021-04-15 15:23:48 -0400

    modal open when a profile is click

--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,27 @@ const seaarchSubmit = document.getElementById("search-submit");
 
 // show 12 item on page
 
-//Modal
-/* 
-let modalDiv = ` <div class="modal-container">
-<div class="modal">
-<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-<div class="modal-info-container">
-<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-<h3 id="name" class="modal-name cap">name</h3>
-<p class="modal-text">email</p>
-<p class="modal-text cap">city</p>
-<hr>
-<p class="modal-text">(555) 555-5555</p>
-<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-<p class="modal-text">Birthday: 10/21/2015</p>
-</div>
-</div>`;
-galleryDiv.insertAdjacentHTML("afterend", modalDiv);
-*/
 window.onload = () => {
   randomUserGenerator();
 };
 // Function to fetch data from random user api
-
+const galleryDiv = document.getElementById("gallery");
 const randomUserGenerator = () => {
   fetch(
     "https://randomuser.me/api/?results=12&nat=us&inc=name,email,location,picture,cell,dob,nat"
   )
     .then((response) => response.json())
-    .then((data) => showRandomuserData(data.results));
+    .then((data) => {
+      showRandomuserData(data.results);
+      cardSelectModalOpen(data);
+      createModal(data);
+    });
 };
 
 function showRandomuserData(data) {
-  data.forEach(function (randomUser, i) {
-    const galleryDiv = document.getElementById("gallery");
-    const galleryCard = `
+  data.forEach(function showInd(randomUser, i) {
+    //const galleryDiv = document.getElementById("gallery");
+    let galleryCard = `
     <div class="card">
 <div class="card-img-container">
 <img class="card-img" src= '${randomUser.picture.large}' alt="profile picture">
@@ -55,26 +41,77 @@ function showRandomuserData(data) {
 <p class="card-text cap">${randomUser.location.city}, ${randomUser.location.state}</p>
 </div>
 </div>`;
+
     galleryDiv.insertAdjacentHTML("beforeend", galleryCard);
   });
 }
+// Click a Card the Modal Opens up
+function cardSelectModalOpen(data) {
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", (e) => {
+      document.querySelector(".modal-container").style.display = "block";
+    });
+  }
+}
 
-// Helper Function
-/* 
-showRandomuserData = (randomUser) => {
-  document.querySelector(
-    ".card-img"
-  ).src = `${randomUser.results[0].picture.large}`;
-  document.getElementById(
-    "name"
-  ).innerText = `${randomUser.results[0].name.first} ${randomUser.results[0].name.last}`;
+//Modal
+function createModal() {
+  let modalDiv = ` <div class="modal-container">
+<div class="modal">
+<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
 
-  document.querySelector(
-    ".card-text"
-  ).innerText = `${randomUser.results[0].email}`;
+</div>`;
+  galleryDiv.insertAdjacentHTML("afterend", modalDiv);
+  const modalContainer = document.querySelector(".modal-container");
+  let closeBtn = document.getElementById("modal-close-btn");
 
-  document.querySelector(
-    ".card-text cap"
-  ).innerText = `${randomUser.results[0].location.city} , ${randomUser.results[0].location.state}`;
+  modalContainer.style.display = "none";
+  closeBtn.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+  });
+}
+
+/*
+let modalInfo = ` 
+<div class="modal-info-container">
+<img class="modal-img" src='${randomUser.picture.large}' alt="profile picture">
+<h3 id="name" class="modal-name cap">${randomUser.name.first} ${randomUser.name.last}</h3>
+<p class="modal-text">${randomUser.email}</p>
+<p class="modal-text cap">${randomUser.location.city}</p>
+<hr>
+<p class="modal-text">${randomUser.cell}</p>
+<p class="modal-text"> ${randomUser.street}.,  ${randomUser.location.city}, ${randomUser.location.state} 
+${randomUser.location.postcode}</p>
+<p class="modal-text">Birthday: ${randomUser.dob.date}</p>
+</div>
+`;
+*/
+
+/*
+let modalOpen = document.querySelector(".card-info-container");
+console.log(modalOpen);
+let modal = document.querySelector(".modal");
+let closeBtn = document.getElementById("modal-close-btn");
+console.log(modal);
+*/
+/*
+gallery.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+*/
+//closeBtn.onclick = function () {
+// modal.style.display = "none";
+//};
+/*
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 };
 */
